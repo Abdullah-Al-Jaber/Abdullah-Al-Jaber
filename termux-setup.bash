0fe648655d39ead697b0b10a01d4598300d6415d
Abdullah Al Jaber <abdullah.0.al.0.jaber@gmail.com>
2024-01-19 11:01:27 +0000

    ok

--- termux-setup.bash
+++ termux-setup.bash
@@ -18,28 +18,24 @@ cd ~
 ### Configuration 
 
 #Fish
-echo -e "\nConfiguring FISH ...\n"
 
 chsh -s fish
 fish -c 'echo "starship init fish | source" >> ./.config/fish/config.fish'
 
 fish -c "set -U fish_greeting "
 
-echo -e "\nConfigured FISH.\n"
-
 #User Bin Folder 
 mkdir ./usr-bin
 fish -c "fish_add_path ./usr-bin"
+export PATH=$PATH:~/usr-bin
 
 #Proot Debian
-echo -e "\nSetting up Debian Distro ...\n"
 
 proot-distro install debian
 ln -s /storage/emulated/0 ./android
 touch ./usr-bin/debian
 echo 'proot-distro login --bind ~/android:/root/android debian -- "$@" '>> ./usr-bin/debian
 chmod +x ./usr-bin/debian
-export PATH=$PATH:~/user-bin
 
 debian sed -i 's/^/#/' /etc/apt/sources.list
 debian echo 'deb [signed-by="/usr/share/keyrings/debian-archive-keyring.gpg"] http://deb.debian.org/debian testing main contrib non-free' >> /etc/apt/sources.list
@@ -50,8 +46,6 @@ debian apt install -y fish npm pip python
 debian apt autoremove -y
 debian chsh -s /usr/bin/fish
 debian fish -c "set -U fish_greeting -e '\nWelcome to Debian (testing)\n'"
-
-echo -e "\nFinished Setting up Debian Distro. \n"
 
 #Misc
 rm ../usr/etc/motd
