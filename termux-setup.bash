0ce65c1a220c439c135986f1eae0f7533d39fb18
Abdullah Al Jaber <abdullah.0.al.0.jaber@gmail.com>
2024-01-19 08:16:03 +0000

    Good

---
+++ termux-setup.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Variable declaration
+ver="1.0.0"
+pwd= pwd
+
+# Start up
+echo -e "\n# Termux Setup Script v$ver\n\n"
+
+apt update
+yes | pkg upgrade
+
+# Install Package 
+yes | pkg install fish starship proot-distro 
+
+cd ~
+
+### Configuration 
+
+#Fish
+
+chsh -s fish
+echo "starship init fish | source" >> ./config/fish/config.fish
+
+fish -c "fish_greeting -U "
+
+#Misc
+rm ../usr/etc/motd
+
+
+cd $pwd
+echo -e "\n# Finished Setup. \n\n"
