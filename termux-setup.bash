dc70835b21bfd9a11c84868e64655c81e4867911
Abdullah Al Jaber <abdullah.0.al.0.jaber@gmail.com>
2024-01-19 09:02:15 +0000

    ok

--- termux-setup.bash
+++ termux-setup.bash
@@ -18,16 +18,28 @@ cd ~
 ### Configuration 
 
 #Fish
+echo -e "\nConfiguring FISH ...\n"
 
 chsh -s fish
 fish -c 'echo "starship init fish | source" >> ./.config/fish/config.fish'
 
 fish -c "set -U fish_greeting "
 
+echo -e "\nConfigured FISH.\n"
+
 #User Bin Folder 
 mkdir ./usr-bin
 fish -c "fish_add_path ./usr-bin"
 
+#Proot Debian
+echo -e "\nSetting up Debian Distro ...\n"
+
+proot-distro install debian
+touch ./usr-bin/debian
+echo "proot-distro login --bind /storage/emulated/0/:/root/android debian" >> ./usr-bin/debian
+chmod +x ./usr-bin/debian
+
+echo -e "\nFinished Setting up Debian Distro. \n"
 
 #Misc
 rm ../usr/etc/motd
