85316ac3913f7cbaea40bd45979b0fa46a98ec2f
Abdullah Al Jaber <abdullah.0.al.0.jaber@gmail.com>
2024-05-04 18:58:49 +0600

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -4,6 +4,6 @@
 
 # Download Script from my GitHub
 
-bash <(curl -s https://raw.githubusercontent.com/abdullah-al-jaber/abdullah-al-jaber/vanilla/Scripts/download_script.bash)
+bash -c 'bash <(curl -s https://raw.githubusercontent.com/abdullah-al-jaber/abdullah-al-jaber/vanilla/Scripts/download_script.bash)'
 
 #
