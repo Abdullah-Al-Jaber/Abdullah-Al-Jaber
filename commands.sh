86adeeb8c0032759044821b7029d3d23f33bdb1e
Abdullah Al Jaber <abdullah.0.al.0.jaber@gmail.com>
2024-05-04 15:00:18 +0600

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -4,6 +4,6 @@
 
 # Download Script from my GitHub
 
-curl -s -o xyz https://raw.githubusercontent.com/abdullah-al-jaber/abdullah-al-jaber/vanilla/Script/download_script.bash && bash ./xyz && rm ./xyz
+curl -s -o xyz https://raw.githubusercontent.com/abdullah-al-jaber/abdullah-al-jaber/vanilla/Scripts/download_script.bash && bash ./xyz && rm ./xyz
 
 #
