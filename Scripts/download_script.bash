94cb46c9c6cd71b7dfd54ba8a19d0f492190f856
Abdullah Al Jaber <abdullah.0.al.0.jaber@gmail.com>
2024-05-05 11:16:49 +0600

    Add files via upload

---
+++ Scripts/download_script.bash
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+### Variable ###
+PWD=$(pwd)
+URL_PREFIX="https://raw.githubusercontent.com/abdullah-al-jaber/abdullah-al-jaber/vanilla/Scripts"
+RED="\e[31m"
+YELLOW="\e[33m"
+GREEN="\e[32m"
+BLUE="\e[34m"
+RESET="\e[0m"
+
+BUFFER="hellou"
+
+download_file() {
+  filename="$1"
+  url="${URL_PREFIX}/${filename}"
+  path="${PWD}/${filename}"
+
+  if [ -f "${path}" ]; then
+    echo -e "${YELLOW}File ${filename} already exists.${RESET}"
+    echo -e "${RED}Removing it !${RESET}"
+    rm "${path}"
+  fi
+
+  # check if input given 
+  if [ -z "$1" ]; then
+    echo
+    read -e -i "$BUFFER" -p "[34mPlease input script name : [0m" filename
+    BUFFER=$filename
+    download_file "${filename}"
+    return
+  fi
+
+  echo -e "${YELLOW}Downloading ($BLUE${url}$RESET$YELLOW)${RESET}"
+  code=$(curl -sL -w "%{http_code}\n"  -o "${path}" "${url}")
+
+  if [[ $code -ne 200 ]]; then 
+    echo -e "${RED}Failed to download (${filename}) ! ${RESET}"
+    rm "${path}"
+    download_file
+    return
+  fi 
+
+  echo -e "${GREEN}Download complete !${RESET}"
+}
+
+download_file
+chmod +x "$path"
