da97c2f53f66335a30dd2fce07e5ffa033cb93e1
Abdullah Al Jaber <abdullah.0.al.0.jaber@gmail.com>
2024-08-01 01:11:47 +0600

    Update warp-plus.py

--- PyScripts/warp-plus.py
+++ PyScripts/warp-plus.py
@@ -10,15 +10,15 @@ import time
 
 red = "\033[0;31m"
 blue = "\033[0;34m"
-bold = "\033[1m"
 yellow = "\033[0;33m"
+bold = "\033[1m"
 reset = "\033[0m"
 
 error_sign = red + bold + "[!]" + reset
 info_sign = blue + bold + "[•]" + reset
 
 version = "1.0.0"
-window_title = "Cracked.io"
+window_title = name = "Help me"
 
 save_file = "./data.json"
 data = {
@@ -27,6 +27,9 @@ data = {
   "bad_request": 0,
   "interval": 10
 }
+good = 0
+bad = 0
+
 
 try:
   import httpx
@@ -74,6 +77,8 @@ def gen_ran_num(length):
 def send_request():
   global data
   global api_url
+  global good
+  global bad
   try:
     install_id = gen_ran_str(22)
     body = {
@@ -99,11 +104,16 @@ def send_request():
     if response.status_code == 200:
       print(f"{info_sign} Request sent successfully !")
       data["good_request"] += 1
+      good += 1
+      time.sleep(1)
+
     else:
       print(f"{error_sign} Failed to send request (server) !")
       print(f"{error_sign} Status code : {response.status_code}")
       print(f"\n{bold}[ Response ]{reset}\n{response.text}\n")
       data["bad_request"] += 1
+      bad += 1
+      time.sleep(5)
 
   except Exception as error:
     print(f"{error_sign} Failed to send request (client) !")
@@ -112,7 +122,7 @@ def send_request():
 
 def show_menu():
   clear_screen()
-  print(f"\t{bold}Cracked.io {blue}v{version}{reset}\n")
+  print(f"\t{bold}{name} {blue}v{version}{reset}\n")
   print(f"{yellow}[1]{reset} Start the script")
   print(f"{yellow}[2]{reset} Set Refferer ID")
   print(f"{yellow}[3]{reset} Set Request Intervals")
@@ -122,22 +132,21 @@ def show_menu():
   print(f"\n\t{bold}[ Press Ctrl +C to exit ]{reset}\n")
 
   code = input(f"\n{yellow}[?]{reset} Select an option : ")
-  # check if code a is number within 1-6
+
   if code.isdigit() and 1 <= int(code) <= 5:
     run(int(code))
   else:
     timer(5,f"{error_sign} Invalid option !")
-    time.sleep(5)
     show_menu()
 
 def timer(seconds,message):
-  for i in range(seconds, 0, -1):
+  for i in range(seconds, -1, -1):
     print(f"\r{message} {bold}[{i}s]{reset}", end="", flush=True)
     time.sleep(1)
 
 def show_instruction():
   clear_screen()
-  print(f"\t{bold}Cracked.io {blue}v{version}{reset}\n")
+  print(f"\t{bold}{name} {blue}v{version}{reset}\n")
   print(f"{blue}[1]{reset} Open 1.1.1.1 App.")
   print(f"{blue}[2]{reset} Go to {bold}Settings > Advanced > Diagnostics{reset}.")
   print(f"{blue}[3]{reset} Copy {bold}ID{reset} from {bold}Client Configuration{reset}.")
@@ -152,34 +161,36 @@ def show_instruction():
 def run(code):
   global data
   if code == 1:
+    global good
+    global bad
     while True:
       clear_screen()
       if data["referrer"] == "":
-        print(f"\t{bold}Cracked.io {blue}v{version}{reset}\n")
+        print(f"\t{bold}{name} {blue}v{version}{reset}\n")
         print("If you don't understand how to get the ID, read the instructions !")
         data["referrer"] = input(f"\n{yellow}[?]{reset} Enter Refferer ID : ")
         clear_screen()
-      print(f"\t{bold}Cracked.io {blue}v{version}{reset}\n")
+      print(f"\t{bold}{name} {blue}v{version}{reset}\n")
       print(f"{info_sign} Current ID : {data['referrer']}")
-      print(f"{info_sign} Good Requests : {data['good_request']}")
-      print(f"{info_sign} Bad Requests : {data['bad_request']}")
+      print(f"{info_sign} Good Requests : {good}")
+      print(f"{info_sign} Bad Requests : {bad}")
       print(f"\n{yellow} Total : {bold}{data['good_request']} GB {reset}")
       print("\n------------\n")
-      print(f"{info_sign} Sending Request to Server ...")
+      timer(data["interval"],f"{yellow}[•]{reset} Next Request in ")
+      print(f"\n{info_sign} Sending Request to Server ...")
       send_request()
       save_data()
-      timer(data["interval"],f"{info_sign} Next Request in ")
   elif code == 2:
     data["referrer"] = input(f"\n{yellow}[•]{reset} Enter Refferer ID : ")
+    data['good_request'] = 0
+    data['bad_request'] = 0
     show_menu()
   elif code == 3:
     interval = input(f"\n{yellow}[•]{reset} Enter Request Intervals (in seconds) : ")
     if interval.isdigit():
-      if int(interval) > 0:
-        data["interval"] = int(interval)
+      data["interval"] = int(interval)
     else:
-      timer(5,f"{error_sign} Invalid interval !")
-      time.sleep(5)
+      timer(5,f"{error_sign} Invalid interval (only positive integers) !")
     show_menu()
   elif code == 4:
     save_data()
