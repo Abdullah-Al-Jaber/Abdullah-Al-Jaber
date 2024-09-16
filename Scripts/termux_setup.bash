3e31fd6b462045e1c6f07ef4ada18166228748ed
Abdullah Al Jaber <abdullah.0.al.0.jaber@gmail.com>
2024-09-16 10:42:16 +0600

    Update termux_setup.bash

--- Scripts/termux_setup.bash
+++ Scripts/termux_setup.bash
@@ -1,6 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-bash -c 'curl -s -H "Cache-Control: no-cache" https://raw.githubusercontent.com/abdullah-al-jaber/abdullah-al-jaber/vanilla/PyScripts/setup_script.py > setup.py'
+bash -c 'curl -s -H "Cache-Control: no-cache" https://raw.githubusercontent.com/abdullah-al-jaber/abdullah-al-jaber/vanilla/PyScripts/termux_setup_script.py > setup.py'
 pkg update
 yes | pkg upgrade
 yes | pkg install python3
