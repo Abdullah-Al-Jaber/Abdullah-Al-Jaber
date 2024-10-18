c92a4d86e35ac1f30f7e009c7cf45169bd8e7dd6
Abdullah Al Jaber <abdullah.0.al.0.jaber@gmail.com>
2024-10-18 06:55:25 +0000

    iklp

--- SCRIPTS/bash/termux-setup.bash
+++ SCRIPTS/bash/termux-setup.bash
@@ -1,10 +1,8 @@
 #!/data/data/com.termux/files/usr/bin/bash
-
-# bash script for termux setup
 yes | pkg update
 yes | pkg upgrade
 
 pkg install python curl
 
-curl -s -H "Cache-Control: no-cache" https://raw.githubusercontent.com/abdullah-al-jaber/abdullah-al-jaber/vanilla/scripts/python/termux-setup.py > termux-setup.py
+curl -s -H "Cache-Control: no-cache" https://raw.githubusercontent.com/abdullah-al-jaber/abdullah-al-jaber/vanilla/SCRIPTS/python/termux-setup.py > termux-setup.py
 python termux-setup.py
--- SCRIPTS/bash/termux_setup.bash
+++
@@ -1,7 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-
-bash -c 'curl -s -H "Cache-Control: no-cache" https://raw.githubusercontent.com/abdullah-al-jaber/abdullah-al-jaber/vanilla/PyScripts/termux_setup.py > termux_setup.py'
-pkg update
-yes | pkg upgrade
-yes | pkg install python3
-python3 termux_setup.py
