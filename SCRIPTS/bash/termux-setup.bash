c5feb4d9413be1fc70adbc922d3a6ef3f6dea278
Abdullah Al Jaber <abdullah.0.al.0.jaber@gmail.com>
2024-10-18 17:03:57 +0600

    Update termux-setup.bash

--- SCRIPTS/bash/termux-setup.bash
+++ SCRIPTS/bash/termux-setup.bash
@@ -2,7 +2,7 @@
 yes | pkg update
 yes | pkg upgrade
 
-yes | pkg install python pip
+yes | pkg install python python-pip
 
 curl -s https://raw.githubusercontent.com/abdullah-al-jaber/abdullah-al-jaber/vanilla/SCRIPTS/python/termux-setup.py > termux-setup.py
 python termux-setup.py
