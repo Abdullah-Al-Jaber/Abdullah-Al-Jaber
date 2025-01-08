5e5a84000766dbdb3e8a36649b3f177b588c0c23
Abdullah Al Jaber <abdullah.0.al.0.jaber@gmail.com>
2025-01-08 21:26:22 +0600

    Delete SCRIPTS directory

--- SCRIPTS/bash/termux-setup.bash
+++
@@ -1,8 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-yes | pkg update
-yes | pkg upgrade
-
-yes | pkg install python python-pip
-
-curl -s https://raw.githubusercontent.com/abdullah-al-jaber/abdullah-al-jaber/vanilla/SCRIPTS/python/termux-setup.py > termux-setup.py
-python termux-setup.py
